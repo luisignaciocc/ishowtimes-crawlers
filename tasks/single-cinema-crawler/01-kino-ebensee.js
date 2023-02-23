bb85d530136ca8306bbc0fc23601025d21b086e4
Luis Ignacio Cabezas <luisignacioccp@gmail.com>
2023-02-23 19:25:08 -0300

    Fisr crawler

---
+++ tasks/single-cinema-crawler/01-kino-ebensee.js
@@ -0,0 +1,27 @@
+const CrawlE = require("crawl-e/v0.5.2");
+
+let crawlE = new CrawlE({
+  cinemas: [
+    {
+      name: "Kulturverein Kino Ebensee",
+      address: "Schulgasse 6, 4802 Ebensee",
+      website: "https://www.kino-ebensee.at/",
+      phone: "0043 6133 6308",
+    },
+  ],
+  showtimes: {
+    url: "https://www.kino-ebensee.at/kinoprogramm.html",
+    movies: {
+      box: ".eventWrap",
+      title: ".eventHeader a",
+      //   For some reason, it's only generating max two showtimes per movie 🤔
+      showtimes: {
+        box: ".date",
+        datetimeFormat: "dd, DD.MM.YY HH:mm [Uhr]",
+        dateLocale: "de",
+      },
+    },
+  },
+});
+
+crawlE.crawl();

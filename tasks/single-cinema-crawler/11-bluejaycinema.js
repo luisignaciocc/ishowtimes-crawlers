446506d3ac12eff5d9be0e134e4d291ab2e30bbf
Luis Ignacio Collantes <luisignacioccp@gmail.com>
2023-02-25 01:00:08 -0300

    Add task 11

---
+++ tasks/single-cinema-crawler/11-bluejaycinema.js
@@ -0,0 +1,27 @@
+// NOT GETTING MOVIES - CSR?
+
+const CrawlE = require("crawl-e/v0.5.2");
+
+let crawlE = new CrawlE({
+  cinemas: [
+    {
+      name: "Blue Jay Cinema",
+      website: "https://bluejaycinema.com/",
+      address: "27315 North Bay Rd. Blue Jay, CA 92317",
+      phone: "(909) 337-3997",
+    },
+  ],
+  movies: {
+    list: {
+      url: "https://bluejaycinema.com/wp-admin/admin-ajax.php",
+      postData:
+        "action=movies-filter&per_page=10&date=:date:&theatres=&cat=&sorting=release&list_all_events=&current_page=",
+      urlDateFormat: "DD.MM.YYYY",
+      box: ".event_list",
+      title: ".movie_title a",
+      postId: ".event_list @id",
+    },
+  },
+});
+
+crawlE.crawl();

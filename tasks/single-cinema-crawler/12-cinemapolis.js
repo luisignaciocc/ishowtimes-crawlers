161702f555253235056eb07d2fce3561719a6870
Luis Ignacio Cabezas <luisignacioccp@gmail.com>
2023-02-25 11:51:46 -0300

    Add test 12

--- tasks/single-cinema-crawler/12-cinemapolis.js
+++ tasks/single-cinema-crawler/12-cinemapolis.js
@@ -21,7 +21,7 @@ let crawlE = new CrawlE({
         anyShowingBadgeIds: null,
         everyShowingBadgeIds: [null],
         skip: false,
-        resultVersion: "4072224857",
+        resultVersion: "1203353629",
       },
       query:
         "query ($date: String, $movieId: ID, $titleClassId: ID, $titleClassIds: [ID], $everyShowingBadgeIds: [ID], $anyShowingBadgeIds: [ID], $resultVersion: String) {\n  showingsForDate(\n    date: $date\n    movieId: $movieId\n    titleClassId: $titleClassId\n    titleClassIds: $titleClassIds\n    everyShowingBadgeIds: $everyShowingBadgeIds\n    anyShowingBadgeIds: $anyShowingBadgeIds\n    resultVersion: $resultVersion\n  ) {\n    data {\n      id\n      time\n      overrideSeatChart\n      overridePriceCard\n      published\n      ticketsSold\n      ticketsPaid\n      current\n      past\n      overrideReservedSeating\n      overrideReservedSeatingValue\n      customHeldSeatCount\n      overrideHeldSeatCount\n      overrideShowingBadges\n      allowWithoutMembership\n      private\n      screenId\n      priceCardId\n      customPriceCardId\n      movie {\n        id\n        name\n        showingStatus\n        displayMetaData\n        urlSlug\n        posterImage\n        signageDisplayPoster\n        bannerImage\n        signageDisplayBanner\n        animatedPosterVideo\n        signageDisplayAnimatedPoster\n        signageMessageOverride\n        color\n        synopsis\n        starring\n        directedBy\n        producedBy\n        searchTerms\n        duration\n        genre\n        allGenres\n        rating\n        ratingReason\n        trailerYoutubeId\n        trailerVideo\n        signageDisplayTrailer\n        releaseDate\n        predictedWeekOneTicketSales\n        tmdbPopularityScore\n        tmdbId\n        includeInComingSoon\n        includeInFuture\n        overridePriceCard\n        sendRentrak\n        rentrakName\n        allowPastSales\n        dcmEdiMovieId\n        dcmEdiMovieName\n        disableOnlineConcessions\n        hasFlatFee\n        termPercentHundredths\n        flatFee\n        titleClassId\n        customPriceCardId\n        __typename\n      }\n      showingBadgeIds\n      predictedAttendance\n      seatsRemaining\n      seatsRemainingWithoutSocialDistancing\n      __typename\n    }\n    count\n    resultVersion\n    __typename\n  }\n}\n",
@@ -31,25 +31,39 @@ let crawlE = new CrawlE({
   hooks: {
     configureRequest: (request, context) => {
       request = request.set(
-        "cookie",
-        "ahoy_visit=f916b773-cd16-42d4-b1df-a1bcdc146494; ahoy_visitor=e28bfed3-7955-4ab2-a3d6-a78cfce332e4; site_id=eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBZ2lCek13IiwiZXhwIjpudWxsLCJwdXIiOiJjb29raWUuc2l0ZV9pZCJ9fQ%3D%3D--6bd828855450066b6db17cb9805e94f4f91057d8; circuit_id=eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBKQT09IiwiZXhwIjpudWxsLCJwdXIiOiJjb29raWUuY2lyY3VpdF9pZCJ9fQ%3D%3D--003b76797e7ad985e98c6330a99222e0477d714c; hardware_id=eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBZ3ciLCJleHAiOm51bGwsInB1ciI6ImNvb2tpZS5oYXJkd2FyZV9pZCJ9fQ%3D%3D--3c0f2f0ceb377fc53b9042799b34d7347c942957"
+        "sec-ch-ua",
+        `"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"`
       );
+      request = request.set("circuit-id", "31");
+      request = request.set("sec-ch-ua-mobile", "?0");
+      request = request.set(
+        "User-Agent",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+      );
+      request = request.set("content-type", "application/json");
+      request = request.set("accept", "*/*");
+      request = request.set("is-electron-mode", "false");
+      request = request.set("client-type", "consumer");
+      request = request.set("site-id", "30");
+      request = request.set("sec-ch-ua-platform", `"Linux"`);
+      request = request.set("host", "cinemapolis.org");
       return request;
     },
     handleShowtimesResponse: (response, context, callback) => {
-      let data = JSON.parse(response.text);
-      let showtimes = [
-        { movie_title: "test", start_at: "2023-02-24T20:00:00", is_3d: false },
-      ];
-      //   let showtimes = data.map((showtime) => {
-      //     return {
-      //       movie_title: showtime.movie.name,
-      //       start_at: showtime.time,
-      //       is_3d: false,
-      //     };
-      //   });
-      console.log(data);
-      callback(null, showtimes);
+      const showtimes =
+        JSON.parse(response.text).data.showingsForDate?.data || [];
+      // let formattedShowtimes = [
+      //   { movie_title: "test", start_at: "2023-02-24T20:00:00", is_3d: false },
+      // ];
+      let formattedShowtimes = showtimes.map((showtime) => {
+        return {
+          movie_title: showtime.movie.name,
+          start_at: showtime.time,
+          is_3d: false,
+        };
+      });
+      console.log(showtimes);
+      callback(null, formattedShowtimes);
     },
   },
 });

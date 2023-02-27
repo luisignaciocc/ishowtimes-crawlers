5d12380876d9e9a372fe7c3c3acd1e9655119037
Luis Ignacio Cabezas <luisignacioccp@gmail.com>
2023-02-27 00:03:21 -0300

    14

--- tasks/cinema-chain-crawler/13-castellolopescinemas.js
+++ tasks/cinema-chain-crawler/13-castellolopescinemas.js
@@ -9,12 +9,20 @@ let crawlE = new CrawlE({
       website: {
         selector: "h3 a",
         attribute: "href",
-        mapper: (value) => `https://castellolopescinemas.pt${value}`,
+        mapper: (value) =>
+          value.startsWith("http")
+            ? value
+            : `https://castellolopescinemas.pt${value}`,
       },
       phone: "div:nth-child(2) > div > div.et_pb_blurb_container",
       email: "div:nth-child(3) > div > div.et_pb_blurb_container",
       address: "div:nth-child(4) > div > div.et_pb_blurb_container",
     },
+  },
+  showtimes: {
+    // I NEED TO USE HERE A PARAMETER THAT IS THIS SAME PAGE, BUT I CAN'T SELECT IT FROM HERE
+    url: "https://castellolopescinemas.pt/wp-json/sessions/cinema?cinema=:cinema.name:&date=2023-02-28",
+    urlDateFormat: "YYYY-MM-DD",
   },
 });
 

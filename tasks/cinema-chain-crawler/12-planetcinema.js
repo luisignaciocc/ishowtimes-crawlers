5f41131bddd3ed313f63905962be9b55a52219bb
Luis Ignacio Cabezas <luisignacioccp@gmail.com>
2023-02-27 00:22:03 -0300

    Finish task 12

--- tasks/cinema-chain-crawler/12-planetcinema.js
+++ tasks/cinema-chain-crawler/12-planetcinema.js
@@ -7,11 +7,29 @@ let crawlE = new CrawlE({
       box: ".cinemas-list ul li",
       name: "a",
       website: "a @href",
+      slug: {
+        selector: "a",
+        attribute: "href",
+        mapper: (value) => value.replace("http://www.planetcinema.pl/", ""),
+      },
     },
     details: {
       url: ":cinema.website:/kontakt",
       phone: ".header-upper-bar p.white a",
       address: ".address",
+    },
+  },
+  showtimes: {
+    url: "https://www.planetcinema.pl/:cinema.slug:/repertuar/date/:date:",
+    urlDateFormat: "YYYY-MM.DD",
+    postData: "day=2023-03-02",
+    movies: {
+      box: ".movie-container",
+      title: ".title",
+      showtimes: {
+        box: ".schedule-hours a",
+        timeFormat: "HH:mm",
+      },
     },
   },
 });
